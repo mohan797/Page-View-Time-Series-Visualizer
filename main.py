b1c035624268f495215bbb48a6907e82c7ff810a
MOHAN KOTESWAR RAO MEKANABOYINA <mohankoteswararao408@gmail.com>
2025-07-04 14:52:50 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# This file is for running functions manually
+
+import time_series_visualizer as ts
+
+# Uncomment the functions below one at a time to generate the plots
+
+ts.draw_line_plot()
+ts.draw_bar_plot()
+ts.draw_box_plot()
